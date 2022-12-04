e45af276dfce4ce6d5ef22db59bc1452bb26b6fa
suucode <limmm.mary@gmail.com>
2022-12-05 06:47:57 +0900

    join시 토큰생성이 안됨

--- src/main/java/shop/ggamf/ggamf/config/auth/LoginUserService.java
+++ src/main/java/shop/ggamf/ggamf/config/auth/LoginUserService.java
@@ -1,5 +1,31 @@
 package shop.ggamf.ggamf.config.auth;
 
-public class LoginUserService {
-    
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import shop.ggamf.ggamf.domain.user.User;
+import shop.ggamf.ggamf.domain.user.UserRepository;
+
+public class LoginUserService implements UserDetailsService{
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        log.debug("디버그 : loadUserByUsername 실행됨");
+        Optional<User> userOP = userRepository.findByUsername(username);
+        if (userOP.isPresent()) {
+            return new LoginUser(userOP.get());
+        } else {
+            return null;
+        }
+
+    }
 }
