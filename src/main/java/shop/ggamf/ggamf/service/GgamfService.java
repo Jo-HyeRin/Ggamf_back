4de7fe89696ab3a2f1c6edcd74e73f49284ebd2a
Hyerinism:) <112357320+Jo-HyeRin@users.noreply.github.com>
2022-12-07 17:06:06 +0900

    [조혜린] 겜프 요청 & Junit Test 완료
    * 겜프요청 완료
    
    * 겜프요청 junit test 완료
    
    * 겜프요청 junit test 수정 완료

--- src/main/java/shop/ggamf/ggamf/service/GgamfService.java
+++ src/main/java/shop/ggamf/ggamf/service/GgamfService.java
@@ -1,11 +1,52 @@
 package shop.ggamf.ggamf.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import shop.ggamf.ggamf.config.exception.CustomApiException;
+import shop.ggamf.ggamf.domain.follow.Follow;
+import shop.ggamf.ggamf.domain.follow.FollowRepository;
+import shop.ggamf.ggamf.domain.user.User;
+import shop.ggamf.ggamf.domain.user.UserRepository;
+import shop.ggamf.ggamf.dto.GgamfReqDto.FollowGgamfReqDto;
+import shop.ggamf.ggamf.dto.GgamfRespDto.FollowGgamfRespDto;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class GgamfService {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public FollowGgamfRespDto 겜프요청(FollowGgamfReqDto followGgamfReqDto) {
+        log.debug("디버그 : 파티방생성 서비스 호출");
+        // 나
+        User follower = userRepository.findById(followGgamfReqDto.getFollowerId())
+                .orElseThrow(() -> new CustomApiException("내 유저 정보가 없습니다", HttpStatus.FORBIDDEN));
+        // 요청받은사람
+        User following = userRepository.findById(followGgamfReqDto.getFollowingId())
+                .orElseThrow(() -> new CustomApiException("해당 유저가 없습니다", HttpStatus.FORBIDDEN));
+        // // 검증 - 고민중
+        // if (followRepository.findFollow(followGgamfReqDto.getFollowerId(),
+        // followGgamfReqDto.getFollowingId()) != null) {
+        // throw new CustomApiException("당신은 이미 요청했습니다. 기다리세요.",
+        // HttpStatus.BAD_REQUEST);
+        // } else if (followRepository.findFollow(followGgamfReqDto.getFollowingId(),
+        // followGgamfReqDto.getFollowerId()) != null) {
+        // throw new CustomApiException("상대가 이미 요청했습니다.", HttpStatus.BAD_REQUEST);
+        // }
+        Follow follow = followGgamfReqDto.toEntity(follower, following);
+        Follow followPS = followRepository.save(follow);
+        return new FollowGgamfRespDto(followPS);
+    }
 
 }
