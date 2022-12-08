5543a994cd31b08f1f34e7a3f2aef125464190b7
Jo-HyeRin <sayhyelin12@naver.com>
2022-12-08 11:44:54 +0900

    겜프거절 junit test 완료

--- src/test/java/shop/ggamf/ggamf/web/GgamfApiControllerTest.java
+++ src/test/java/shop/ggamf/ggamf/web/GgamfApiControllerTest.java
@@ -127,4 +127,23 @@ public class GgamfApiControllerTest extends DummyEntity {
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.followId").value(5L));
     }
 
+    @WithUserDetails(value = "ssar", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    public void rejectGgamf_test() throws Exception {
+        // given
+        Long followId = 3L;
+        Long userId = 1L;
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.delete("/s/api/ggamf/user/" + userId + "/reject/" + followId)
+                        .contentType(APPLICATION_JSON_UTF8));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.followId").value(3L));
+    }
+
 }
