c6699a74c2b0063cfd29706c80fe58cd400b318c
suhyeon <112357365+suucode@users.noreply.github.com>
2022-12-14 11:14:04 +0900

    [임수현] 회원 가입 시 중복 체크 기능 & JUnit Test 완료 (#37)
    * join 완료
    
    * join시 토큰생성이 안됨
    
    * login 실행시 토큰생성오류
    
    * login 완료
    
    * [조혜린] 파티방 생성 Test까지 완료
    
    [조혜린] 파티방 생성 Test까지 완료
    
    * UserStateEnum 생성
    
    * pull받기 전
    
    * service 수정
    
    * 자기소개 수정 완료
    
    * userService 중복체크제외 구현완료
    
    * update(사진 base64제외), 회원탈퇴 완료
    
    * user 가입/탈퇴 수정완료
    
    * User domain 수정
    
    * 유저상세보기 service 완료
    
    * pull 받기 전
    
    * userRepositoryQueryTest 완료
    
    * 유저 상세보기 기능완성
    
    * pull 받음
    
    * 유저 상세보기 완료
    
    * Junit test중
    
    * ReportRepositoryQueryTest 성공
    
    * 리포트 한건보기 쿼리 완료
    
    * userApiControllerTest 작성중
    
    * user join/update JUnit test 완료
    
    * user JUnit test 완료
    
    * JwtProcess 토큰 만료 예외처리 구현
    
    * 아이디 중복확인 기능 & JUnit test 완료
    
    * 회원가입 중복체크 & JUnit Test 완료
    
    Co-authored-by: Hyerinism:) <112357320+Jo-HyeRin@users.noreply.github.com>

--- src/main/java/shop/ggamf/ggamf/config/jwt/JwtProcess.java
+++ src/main/java/shop/ggamf/ggamf/config/jwt/JwtProcess.java
@@ -1,6 +1,10 @@
 package shop.ggamf.ggamf.config.jwt;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -8,6 +12,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 
 import shop.ggamf.ggamf.config.auth.LoginUser;
 import shop.ggamf.ggamf.config.enums.UserEnum;
+import shop.ggamf.ggamf.config.exception.CustomApiException;
 import shop.ggamf.ggamf.domain.user.User;
 
 public class JwtProcess {
@@ -25,6 +30,10 @@ public class JwtProcess {
 
     public static LoginUser verify(String token) {
         DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token);
+        Date expiration = decodedJWT.getExpiresAt();
+        if (expiration.before(new Date())) {
+            new CustomApiException("해당토큰은 만료되었습니다.", HttpStatus.BAD_REQUEST);
+        }
         Long id = decodedJWT.getClaim("id").asLong();
         String username = decodedJWT.getClaim("username").asString();
         String role = decodedJWT.getClaim("role").asString();
