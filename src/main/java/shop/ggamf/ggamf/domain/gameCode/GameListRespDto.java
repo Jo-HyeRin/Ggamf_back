39c3840d39adb74294bea0141c378b36e16500b2
suhyeon <112357365+suucode@users.noreply.github.com>
2022-12-15 17:41:40 +0900

    [임수현] 더미데이터 수정
    * dummyDate 수정
    
    Co-authored-by: Hyerinism:) <112357320+Jo-HyeRin@users.noreply.github.com>

---
+++ src/main/java/shop/ggamf/ggamf/domain/gameCode/GameListRespDto.java
@@ -0,0 +1,16 @@
+package shop.ggamf.ggamf.domain.gameCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class GameListRespDto {
+    // private Long id;
+    private String logo;
+    private String gameName;
+}
---
+++ src/main/java/shop/ggamf/ggamf/domain/room/RoomListRespDto.java
@@ -0,0 +1,20 @@
+package shop.ggamf.ggamf.domain.room;
+
+import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoomListRespDto {
+    private BigInteger id;
+    private BigInteger count;
+    private BigInteger totalPeople;
+    private String roomName;
+    private String nickname;
+}
