5e8b198fdf4d99688db809cd4ac3752425d15d3d
suucode <limmm.mary@gmail.com>
2022-12-15 12:38:57 +0900

    방 정보보기 쿼리 & JUnit 테스트 완료

---
+++ src/main/java/shop/ggamf/ggamf/domain/room/RoomRepositoryQuery.java
@@ -0,0 +1,39 @@
+package shop.ggamf.ggamf.domain.room;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import org.qlrm.mapper.JpaResultMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoomRepositoryQuery {
+
+    @Autowired
+    private EntityManager em;
+
+    public List<RoomInfoRespDto> findRoomInfo() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("select r.id, count(*)+1 count, r.total_people, r.room_name, u.nickname from enter e ")
+                .append("inner join room r on r.id = e.room_id ")
+                .append("inner join users u on u.id = r.user_id ")
+                .append("group by r.id");
+
+        Query query = em.createNativeQuery(sb.toString());
+
+        JpaResultMapper result = new JpaResultMapper();
+        try {
+            List<RoomInfoRespDto> roomInfoRespDto = result.list(query, RoomInfoRespDto.class);
+            return roomInfoRespDto;
+        } catch (NoResultException e) {
+            List<RoomInfoRespDto> roomInfoRespDto = new ArrayList<>();
+            return roomInfoRespDto;
+        }
+
+    }
+}
---
+++ src/test/java/shop/ggamf/ggamf/domain/room/RoomRepositoryQueryTest.java
@@ -0,0 +1,106 @@
+package shop.ggamf.ggamf.domain.room;
+
+import javax.persistence.EntityManager;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import shop.ggamf.ggamf.config.dummy.DummyEntity;
+import shop.ggamf.ggamf.domain.enter.Enter;
+import shop.ggamf.ggamf.domain.enter.EnterRepository;
+import shop.ggamf.ggamf.domain.gameCode.GameCode;
+import shop.ggamf.ggamf.domain.gameCode.GameCodeRepository;
+import shop.ggamf.ggamf.domain.user.User;
+import shop.ggamf.ggamf.domain.user.UserRepository;
+
+@Import(RoomRepositoryQuery.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class RoomRepositoryQueryTest extends DummyEntity {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private RoomRepositoryQuery roomRepositoryQuery;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private GameCodeRepository gameCodeRepository;
+
+    @Autowired
+    private EnterRepository enterRepository;
+
+    @BeforeEach
+    public void setUp() {
+        autoincrement_reset();
+        dummy_init();
+    }
+
+    @Test
+    public void findRoomInfo_test() {
+        // given
+
+        // when
+        RoomInfoRespDto roomInfoRespDto = roomRepositoryQuery.findRoomInfo().get(2);
+
+        // then
+        Assertions.assertThat(roomInfoRespDto.getNickname()).isEqualTo("nickcos");
+    }
+
+    private void dummy_init() {
+        User ssar = userRepository.save(newUser("ssar"));
+        User cos = userRepository.save(newUser("cos"));
+        User lala = userRepository.save(newUser("lala"));
+        User dada = userRepository.save(newUser("dada"));
+        User kaka = userRepository.save(newUser("kaka"));
+        User vovo = userRepository.save(newUser("vovo"));
+        User toto = userRepository.save(newUser("toto"));
+        User ohoh = userRepository.save(newUser("ohoh"));
+        User yeye = userRepository.save(newUser("yeye"));
+        User gogo = userRepository.save(newUser("gogo"));
+        User romio = userRepository.save(newUser("romio"));
+        // GameCode : 게임카테고리
+        GameCode etc = gameCodeRepository.save(newGameCode("etc"));
+        GameCode LoL = gameCodeRepository.save(newGameCode("LoL"));
+        GameCode starcraft = gameCodeRepository.save(newGameCode("starcraft"));
+        GameCode battleground = gameCodeRepository.save(newGameCode("battleground"));
+        // Room : 파티방
+        Room endroom1 = roomRepository.save(endRoom("roomname1", ssar, LoL));
+        Room room2 = roomRepository.save(newRoom("roomname2", ssar, etc));
+        Room room3 = roomRepository.save(newRoom("roomname3", cos, LoL));
+        Room endroom4 = roomRepository.save(endRoom("roomname4", lala, etc));
+        Room room5 = roomRepository.save(newRoom("roomname5", yeye, starcraft));
+        Room room6 = roomRepository.save(newRoom("roomname6", ohoh, battleground));
+        // Enter : 방 참여 정보
+        Enter enter1 = enterRepository.save(endEnter(lala, endroom1));
+        Enter enter11 = enterRepository.save(endEnter(dada, endroom1));
+        Enter enter111 = enterRepository.save(endEnter(gogo, endroom1));
+        Enter enter2 = enterRepository.save(newEnter(cos, room2));
+        Enter enter22 = enterRepository.save(newEnter(kaka, room2));
+        Enter enter222 = enterRepository.save(newEnter(romio, room2));
+        Enter enter3 = enterRepository.save(newEnter(ssar, room3));
+        Enter enter33 = enterRepository.save(newEnter(toto, room3));
+        Enter enter333 = enterRepository.save(newEnter(gogo, room3));
+        Enter endEnter4 = enterRepository.save(endEnter(ssar, endroom4));
+        Enter endEnter44 = enterRepository.save(endEnter(cos, endroom4));
+        Enter endEnter444 = enterRepository.save(endEnter(yeye, endroom4));
+        Enter endEnter4444 = enterRepository.save(endEnter(romio, endroom4));
+    }
+
+    private void autoincrement_reset() {
+        this.em
+                .createNativeQuery("ALTER TABLE users ALTER COLUMN `id` RESTART WITH 1")
+                .executeUpdate();
+    }
+}
